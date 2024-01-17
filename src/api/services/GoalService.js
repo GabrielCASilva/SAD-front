5e291796b946225f46c93d30122706aff79fdce2
GabrielCASilva <gabriel.foxcarvalho@gmail.com>
2024-01-17 00:00:02 -0300

    update request logic with auth

--- src/api/services/GoalService.js
+++ src/api/services/GoalService.js
@@ -1,12 +1,12 @@
 import Service, { METHOD } from "../core"
 
-export default class EmployeeService extends Service{
-    static async getEmployees(props) {
+export default class GoalService extends Service{
+    static async getTasks(props) {
         const {signal} = props
          
         const response = await this.call({
             method: METHOD.GET,
-            url: "/funcionarios",
+            url: "/metas",
             signal: signal
         })
         
--- src/components/Modal/modals/PerformanceAppraisalModal.jsx
+++ src/components/Modal/modals/PerformanceAppraisalModal.jsx
@@ -1,9 +1,13 @@
 import Modal from "..";
+import { DatePicker } from 'antd';
+const { RangePicker } = DatePicker
 
 export default function PerformanceAppraisalModal(){
     return (
-        <Modal titleButton="Avaliação de desempenho">
+        <Modal titleButton="Avaliação de desempenho" onClick={()=>{}}>
             <h1>Avaliação de desempenho</h1>
+            <p>Selecione o intervalo de tempo que deseja proceguir com a avaliação</p>
+            <RangePicker defaultValue={[]} format="DD/MM/YYYY"/>
         </Modal>
     )
 }
