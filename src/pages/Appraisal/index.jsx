460896048a515e931812a2064c637045c14ffc13
GabrielCASilva <gabriel.foxcarvalho@gmail.com>
2024-03-13 22:17:47 -0300

    Keep calm and commit

--- src/pages/Appraisal/index.jsx
+++ src/pages/Appraisal/index.jsx
@@ -1,37 +1,36 @@
-import { useLocation } from "react-router-dom";
-import { ReportModal } from "../../components/Modal/modals/ReportModal";
-import { SimpleCard } from "../../components/Card";
-import DetailLayout from "../../layout/DetailLayout";
-import { SubTitle } from "../../components/Titles";
-import { currentDateBRFormat } from "../../utils/date";
-import { useStore } from "../../store";
-import { useEffect } from "react";
+import { useLocation } from 'react-router-dom';
+import { ReportModal } from '../../components/Modal/modals/ReportModal';
+import { SimpleCard } from '../../components/Card';
+import DetailLayout from '../../layout/DetailLayout';
+import { SubTitle } from '../../components/Titles';
+import { currentDateBRFormat } from '../../utils/date';
+import { useStore } from '../../store';
+import { useEffect } from 'react';
 
-export default function Appraisal(){
-  const location = useLocation();
-  const { appraisal } = useStore();
-  const { state } = location;
-  const { name, goal, initialDate, finalDate, dateInterval } = state;
-  
-  useEffect(() => {
-    appraisal.setAppraisal({data: state});
+export default function Appraisal() {
+	const location = useLocation();
+	const { appraisal } = useStore();
+	const { state } = location;
+	const { name, goal, initialDate, finalDate, dateInterval } = state;
 
-  }, []);
+	useEffect(() => {
+		appraisal.setAppraisal({ data: state });
+	}, []);
 
-  const title = `${name} - ${currentDateBRFormat()}`;
-  return (
-    <DetailLayout title={title} card="appraisal">
-      <SimpleCard>
-          <SubTitle>Meta atual</SubTitle>
-          <p>{goal}</p>
-      </SimpleCard>
-      <SimpleCard>
-          <SubTitle>Período calculado</SubTitle>
-          <p>{dateInterval}</p>
-          <p>Início: {initialDate}</p>
-          <p>Final: {finalDate}</p>
-      </SimpleCard>
-      <ReportModal />
-    </DetailLayout>
-  );
+	const title = `${name} - ${currentDateBRFormat()}`;
+	return (
+		<DetailLayout title={title} card="appraisal">
+			<SimpleCard>
+				<SubTitle>Meta atual</SubTitle>
+				<p>{goal}</p>
+			</SimpleCard>
+			<SimpleCard>
+				<SubTitle>Período calculado</SubTitle>
+				<p>{dateInterval}</p>
+				<p>Início: {initialDate}</p>
+				<p>Final: {finalDate}</p>
+			</SimpleCard>
+			<ReportModal />
+		</DetailLayout>
+	);
 }
